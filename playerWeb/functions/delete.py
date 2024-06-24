12d9e5de11f6deb1dfd5aecd611d48b613fa4f6f
Mi_Xi <1095917171@qq.com>
2024-06-24 20:17:39 +0800

    增加删除音乐api

---
+++ playerWeb/functions/delete.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from playerWeb.models import Music, RelUserMusic
+import json
+import os
+
+@csrf_exempt
+def delete_music(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        userid = data.get('userid')
+        musicid = data.get('musicid')
+        print(musicid)
+        filename = Music.objects.filter(id=musicid).first().url.split('/')[-1]
+        RelUserMusic.objects.filter(userid=userid,musicid=musicid).delete()
+        Music.objects.filter(id=musicid).delete()
+        filepath = os.path.join('playerWeb','media','audio', filename)
+        os.remove(filepath)
+        return HttpResponse(status=200)
+    return HttpResponse(status=405)  # Method Not Allowed
--- playerWeb/functions/urls.py
+++ playerWeb/functions/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from playerWeb import views
-from playerWeb.functions import upload
+from playerWeb.functions import upload,delete
 
 router = DefaultRouter()
 router.register('music', views.MusicViewSet)
@@ -11,4 +11,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('uploadmusic/', upload.upload_music, name='upload_music'),
     path('uploadimage/', upload.upload_image, name='upload_image'),
+    path('deletemusic/', delete.delete_music, name='delete_music')
 ]
